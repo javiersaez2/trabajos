d0cfb432b7a29516bcf0af13f6eef8348e2d919d
JavierSaezZ <AS3-1-21@uni.lan>
2019-03-11 13:20:12 +0100

    a

--- 3-evalucion/Ejemplo1/ejemplo.js
+++ 3-evalucion/Ejemplo1/ejemplo.js
@@ -3,7 +3,17 @@ ejempl01
 */
 
 x = 5
-y = 1
+y = 4
 
-z = x + y
-console.log(z)
+z =  Math.floor(Math.random() * 101);
+
+// si la suma es > 10. "es mayor que 10"
+// si no: "es mayor que 10"
+// si = 10: "es igual a 10"
+// pista usa "if / else"
+
+
+
+if (z > 50) {console.log("es mayor que 50")}
+else if (z < 50) {console.log("es menor que 50")}
+else if (z == 50) {console.log("es igual que 50")}
---
+++ 3-evalucion/Ejemplo1/ejemplo2.js
@@ -0,0 +1,20 @@
+/*
+ejempl01
+*/
+
+
+
+// si la suma es > 10. "es mayor que 10"
+// si no: "es mayor que 10"
+// si = 10: "es igual a 10"
+// pista usa "if / else"
+
+
+
+
+z = -1
+while (z != 1000) {
+z = Math.floor(Math.random() * 5000001); 
+console.log ("El valor de z es:" + z)
+
+}
