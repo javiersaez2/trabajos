3512cbe83fd4208a43eca9e0372c94b46766c979
JavierSaezZ <AS3-1-21@uni.lan>
2019-04-09 10:18:48 +0200

    :)

--- 3-evalucion/nodejs/Ejemplo2/app.js
+++ 3-evalucion/nodejs/Ejemplo2/app.js
@@ -5,32 +5,37 @@ app.get('/', function (req, res) {
   res.send('Hola');
 });
 
-app.get('/web', function (req, res) {
-  res.send()
-});
+
 
 tiempo = [
-  {Dia: "Miercoles, 13", cielo: "Chubascos", Temperatura: 9},
-  {Dia: "Jueves, 14", cielo: "Nublado",Temperatura: 12},
-  {Dia: "Viernes, 15", cielo: "Soleado",Temperatura: 15},
-  {Dia: "Sabado, 16", cielo: "Soleado",Temperatura: 11},
-  {Dia: "Domingo, 17", cielo: "Lluvia",Temperatura: 12},
-  {Dia: "Lunes, 18", cielo: "Lluvia",Temperatura: 24},
-  {Dia: "Martes, 19", cielo: "Lluvia", Temperatura: 9},
-  {Dia: "Miercoles, 20", cielo: "Soleado",Temperatura: 16},
-  {Dia: "Jueves, 21", cielo: "Nublado",Temperatura: 12},
-  {Dia: "Viernes, 22", cielo: "Lluvia",Temperatura: 16},
-  {Dia: "Sabado, 23", cielo: "Lluvia",Temperatura: 24},
-  {Dia: "Domingo, 24", cielo: "Nublado",Temperatura: 11},
-  {Dia: "Lunes, 25", cielo: "Lluvia",Temperatura: 11}
+  {Dia: "Miercoles", Nº: "13", cielo: "Chubascos", Temperatura: 9},
+  {Dia: "Jueves", Nº: "14", cielo: "Nublado",Temperatura: 12},
+  {Dia: "Viernes", Nº:"15", cielo: "Soleado",Temperatura: 15},
+  {Dia: "Sabado", Nº:"16", cielo: "Soleado",Temperatura: 11},
+  {Dia: "Domingo", Nº:"17", cielo: "Lluvia",Temperatura: 12},
+  {Dia: "Lunes", Nº:"18", cielo: "Lluvia",Temperatura: 24},
+  {Dia: "Martes", Nº:"19", cielo: "Lluvia", Temperatura: 9},
+  {Dia: "Miercoles", Nº:"20", cielo: "Soleado",Temperatura: 16},
+  {Dia: "Jueves", Nº:"21", cielo: "Nublado",Temperatura: 12},
+  {Dia: "Viernes", Nº:"22", cielo: "Lluvia",Temperatura: 16},
+  {Dia: "Sabado", Nº:"23", cielo: "Lluvia",Temperatura: 24},
+  {Dia: "Domingo", Nº:"24", cielo: "Nublado",Temperatura: 11},
+  {Dia: "Lunes", Nº:"25", cielo: "Lluvia",Temperatura: 11}
   ]
   
-  
-  var i;
-  for (i = 0; i < tiempo.length;i++){
-      if(tiempo[i].cielo  == "Lluvia", tiempo [i].Temperatura  == "24") {
-      console.log (tiempo[i].Dia + " " + tiempo[i].cielo + " " + tiempo [i].Temperatura)} 
-  }
+  app.get('/web', function (req, res) {
+
+  var r=[];
+  for (var i = 0; i < tiempo.length;i++
+    ){
+      if (tiempo[i].Dia  == "Sabado") {
+        r[r.length] = (tiempo[i].Dia + " " + tiempo[i].Nº + " " + tiempo[i].cielo + " " + tiempo [i].Temperatura)
+        
+       
+       
+  }}
+  res.send(r)
+});
   
 
 
